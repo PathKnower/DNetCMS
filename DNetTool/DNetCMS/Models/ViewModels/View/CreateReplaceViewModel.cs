51fbf5ee70acb60b3b13baa613b8693bfa5b37d3
Хайрутдинов Тимур <wardef10@gmail.com>
2018-04-21 12:23:08 +0300

    Add other views to ViewController

--- DNetTool/DNetCMS/Models/ViewModels/View/CreateReplaceViewModel.cs
+++ DNetTool/DNetCMS/Models/ViewModels/View/CreateReplaceViewModel.cs
@@ -10,13 +10,16 @@ namespace DNetCMS.Models.ViewModels.View
     public class CreateReplaceViewModel
     {
         [Required]
+        [Display(Name = "Выбранная страница для перезаписи")]
         public string ChoosenView { get; set; }
 
         public SelectList Views { get; set; }
 
+        [Display(Name = "Активен")]
         public bool Enable { get; set; }
 
         [Required]
+        [Display(Name = "Код страницы")]
         public string Code { get; set; }
     }
 }
